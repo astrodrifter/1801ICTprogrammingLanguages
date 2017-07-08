891bf1bed9ce5417b4b1ce620a6a1df12a3e94cc
astrodrifter <dhruva.oshea@connect.qut.edu.au>
2017-07-08 15:35:18 +1000

    variables and loops

--- eclipse-workspace/ezDemo/src/ezDemo/HelloWorld1.java
+++ eclipse-workspace/ezDemo/src/ezDemo/HelloWorld1.java
@@ -3,9 +3,32 @@ package ezDemo;
 public class HelloWorld1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.printf("Hello World");
-
+		
+		//print hello world
+		System.out.println("Hello World");
+		
+		for (int i = 0; i < 5; i++) {
+			System.out.println(i);
+		}
+		
+		//declare, initialize and print an integer
+		int myNum;
+		myNum = 2;
+		System.out.printf("myNum = " + myNum + "\n");
+		
+		//declare, initialize and print a string
+		String myString = "Word";
+		System.out.println(myString);
+		myString = "New word";   //assign new string to myString
+		System.out.println(myString);
+		
+		//while loop
+		int i = 0;
+		while(i < 5) {
+			System.out.println(i);
+			i++;
+		}
+		
 	}
 
 }
