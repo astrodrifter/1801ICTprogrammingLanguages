04727a305d8efe670256108ec906c12fa4960544
astrodrifter <dhruva.oshea@connect.qut.edu.au>
2017-07-08 14:17:29 +1000

    helloWorld Udemy

---
+++ eclipse-workspace/ezDemo/src/ezDemo/HelloWorld.java
@@ -0,0 +1,5 @@
+package ezDemo;
+
+public class HelloWorld {
+
+}
---
+++ eclipse-workspace/ezDemo/src/ezDemo/HelloWorld1.java
@@ -0,0 +1,11 @@
+package ezDemo;
+
+public class HelloWorld1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.printf("Hello World");
+
+	}
+
+}
