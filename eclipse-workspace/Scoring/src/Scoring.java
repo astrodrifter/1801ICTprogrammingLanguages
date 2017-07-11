f4c4c7f55f41d23239c8863e234cf621c15e7990
astrodrifter <dhruva.oshea@connect.qut.edu.au>
2017-07-11 20:18:44 +1000

    improvements to Scoring project java. Still some runtime bugs.

--- eclipse-workspace/Scoring/src/Scoring.java
+++ eclipse-workspace/Scoring/src/Scoring.java
@@ -3,7 +3,8 @@
  * Purpose: Calculate winner of Olympic diving.
  * By Dhruva O'Shea 08/07/2017
  *
- * Program will randomly generate judges scores then calculate the winner
+ * Program will randomly generate number of contestants and judges, 
+ * randomly generate scores and then calculate the winner.
  * 
  */
 
@@ -13,8 +14,11 @@ public class Scoring {
 	public static void main(String[] args) {
 		
 		/* 2D array declare, initialize, send to method to fill */
-		int contestants = 5;
-		int judges = 7;
+		int contestants;
+		int judges;
+		contestants = getNum();
+		judges = getNum();
+		
 		int myArray2[][] = new int[contestants][judges];
 		fillArray2(myArray2,contestants, judges); //fill array with random values between 0 and 20
 		printArray2(myArray2,contestants, judges); //print again
@@ -24,6 +28,20 @@ public class Scoring {
 		rowAve(myArray2, scores, contestants, judges);
 		findWinner(scores, contestants);
 		
+	}
+	/* generate random number of contestants and judges */
+	static int getNum() {
+		int num;
+		int max = 10;
+		int min = 2;
+		Random rn = new Random();
+		int n = max - min + 1;
+		int rnNum = rn.nextInt() % n;
+		num = Math.abs(rnNum);
+		if(num > 2) {
+			num++;
+		}
+		return num;
 	}
 	/* prints 1D array  */
 	static void printArray1(int[] arr) {
@@ -55,7 +73,9 @@ public class Scoring {
 	}
 	/* prints 2D array  */
 	static void printArray2(int[][] arr, int rows, int cols) {
+		System.out.printf("SCORES\n");
 		for (int j = 0; j < rows; j++) {
+			System.out.printf("Contestant "+(j+1)+": ");
 			for(int k = 0; k < cols; k++) {
 				System.out.printf(" "+arr[j][k]);
 			}
@@ -66,18 +86,20 @@ public class Scoring {
 	/* Find Row Averages */
 	static void rowAve(int[][] arr, int[] scores, int rows, int cols) {
 		int sum;
+		System.out.printf("TOTALS\n");
 		for (int j = 0; j < rows; j++) {
 			sum = 0;
 			for(int k = 0; k < cols; k++) {
 				sum += arr[j][k];
 			}
 			scores[j] = sum/cols;
-			System.out.printf("Contestant " +(j+1)+ " scored " +scores[j]+ "\n");
+			System.out.printf("Contestant " +(j+1)+ ": scored " +scores[j]+ "\n");
 		}
 		
 	}
 	/* find the winner by finding max score in scores array */
 	static void findWinner(int[] arr, int rows) {
+		System.out.printf("RESULT\n");
 		int max = arr[0];
 		int winner = 1;
 		/* find max and winner */
@@ -96,23 +118,23 @@ public class Scoring {
 			}
 		}
 		
-		if(count > 2) {
+		if(count > 1) {
 			int winners[] = new int[rows];
 			for(int i = 0; i < rows; i++) {
 				if(arr[i] == max) {
-					winners[i] = i;
+					winners[i] = 1;
 				
 				}
 			}
-			System.out.printf("Players ");
+			System.out.printf("Contestants ");
 			for(int j = 0; j < rows; j++) {
 				if(winners[j] != 0) {
-					System.out.printf(j+" ");
+					System.out.printf((j+1)+", ");
 				}
 			}
 			System.out.printf(" are equal winners with a score of "+max);
 		} else {
-			System.out.printf("The winner was contestant "+winner+" with "+max+" points.\n");
+			System.out.printf("The winner was contestant "+(winner+1)+" with "+max+" points.\n");
 		}
 	
 		
