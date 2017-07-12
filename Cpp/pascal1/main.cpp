92d7b9b924abad60e0bed6d636ad25284ad17271
astrodrifter <dhruva.oshea@connect.qut.edu.au>
2017-07-12 17:15:49 +1000

    pascal triangle looking good
    takes user input for length and prints blank spaces.

--- Cpp/pascal1/main.cpp
+++ Cpp/pascal1/main.cpp
@@ -6,10 +6,10 @@ using namespace std;
 
 int main(int argc, char **argv)
 {
-    int length = 11;
-    /* get length 
-    printf("Enter length\n");
-    cin >> length; */
+    /* get length */
+    int length;
+    printf("Enter length for Pascal's Triangle\n");
+    scanf("%i",&length);
     
     /* set elements to zero */
     int rows[length][length];
@@ -19,14 +19,24 @@ int main(int argc, char **argv)
         }
     }
     
-    /* first element set to 1 */
-    rows[0][0] = 1;
     /* complete algorithym */
+    int countDown = length-2; //to set spaces for triangle
 	for(int i = 0; i < length; i++) {
+    rows[i][0] = 1; // set first number of each row to 1
+    
+        /* set blank space */
+        int count = countDown; 
+        while(count > 0){
+            printf(" ");
+            count--;
+        }
         for(int j = 0; j < length; j++){
             rows[i+1][j] = rows[i][j-1]+rows[i][j];
-            printf("%i ",rows[i][j]);
+            if(rows[i][j] != 0) {
+                printf("%i ",rows[i][j]);
+            }
         }
+        countDown--;
         printf("\n");
     }
 	return 0;
